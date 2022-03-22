a2d83a984b8a54e4393816fd8fb5bcecaff6dd22
march213 <jane.molodetskaya@gmail.com>
2022-03-21 21:29:44 -0400

    use jazzicon

--- components/EthName.js
+++ components/EthName.js
@@ -1,17 +1,19 @@
 import { useState, useEffect } from 'react';
+import Jazzicon, { jsNumberForAddress } from 'react-jazzicon';
 import { web3 } from '../lib/web3';
 
 const EnsName = function ({ address }) {
   // TODO!
   // check for ENS domain
-  // get image if it has one
-  // make jazzicon if not
 
-  let formattedAddress = address.substr(0, 8) + '...' + address.substr(-4);
+  const formattedAddress = address.substr(0, 8) + '...' + address.substr(-4);
+  const seed = jsNumberForAddress(address);
 
   return (
     <div className="eth-name">
-      <div className="icon">{/* icon goes here */}</div>
+      <div className="icon">
+        <Jazzicon diameter={32} seed={seed} />
+      </div>
 
       <div className="name">
         <span className="primary">{/* ENS name if one here */}</span>
